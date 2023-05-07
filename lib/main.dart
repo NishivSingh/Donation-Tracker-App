439e61b5c43cee87885fe989e5b01aaab643223f
Nishiv Singh <singh.136@iitj.ac.in>
2023-05-07 19:51:57 +0530

    Added the provider app feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,27 @@
-import 'package:duck_cart/util.dart';
 import 'package:flutter/material.dart';
+import 'all_creator_screen.dart';
+import 'creator.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CreatorsProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'TIOBU'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            const Icon(Icons.menu),
-            const SizedBox(width: 50,),
-            Text(widget.title),
-          ],
-        ),
+        home: AllCreatorsScreen(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-        child: ListView(
-          children:[
-            const Padding(
-              padding: EdgeInsets.only(top: 10,bottom: 10,left: 5),
-            child: Text("Recommended Creators",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-              Profile(),Profile(),
-            ],),
-            const SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-              Profile(),Profile(),
-            ],),
-            const SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-              Profile(),Profile(),
-            ],),
-            
-          ],
-        ),
-      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
